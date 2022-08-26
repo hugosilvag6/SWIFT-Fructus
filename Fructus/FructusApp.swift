9ef0e04fd7f7a402140d0f1bc217d8b3a903875d
hugosilvag6 <hugosilvag6@hotmail.com>
2022-08-26 16:11:51 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Hugo Silva on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
